e1f141555e8719f24367b911d838d674a41e49e3
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2015-10-28 14:10:38 -0400

    Add stub 'data' plugins to be filled in later

---
+++ prism-data-crm.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: Prism | CRM Data
+ * Description: Activate to load sample 'CRM' data
+ * Author: PatchWorks
+ * Author URI: https:/patch.works
+ */
+
+// Just a stub plugin for now, will add later
